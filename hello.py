f44d2e78e9a5e020fc2763ffe92586ed04e7dcdb
pyosw0108 <pyosw0108@naver.com>
2021-07-05 14:58:14 +0900

    issue #1

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 #! /usr/bin/env python
 
-print("Hello this is Github")
+import sys
+
+print(sys.argv)
+
+
+name1 = sys.argv[1]
+name2 = sys.argv[2]
+
+print(f"hello {name1}")
+print(f"hello {name2}")
+
